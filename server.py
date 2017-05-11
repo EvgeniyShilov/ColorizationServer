bc4cbb1c4adee57c8946186904f193bbcb5f55bb
EvgeniyShilov <eugshilov@gmail.com>
2017-05-11 22:22:25 +0300

    made quick fix

--- server.py
+++ server.py
@@ -1,22 +1,22 @@
 #!/usr/bin/python
 
 from bottle import *
-from neuralnet/colorize import *
+from neuralnet\colorize import *
 
 @post('/load')
 def load():
     img = request.body.read()
     id = request.get_header("id")
     token = request.get_header("token")
-    f = open("./storage/input/" + id + ".png", "wb")
+    f = open(".\\storage\\input\\" + id + ".png", "wb")
     f.write(img.decode('base64'))
     f.close()
-    colorize("./storage/input/" + id + ".png", "./storage/output/" + id + ".png", token)
+    colorize(".\\storage\\input\\" + id + ".png", ".\\storage\\output\\" + id + ".png", token)
 
 @get("/get")
 def get():
     id = request.get_header("id")
-    f = open("./storage/output/" + id + ".png", "rb")
+    f = open(".\\storage\\output\\" + id + ".png", "rb")
     img = f.read().encode("base64")
     f.close()
     return img
