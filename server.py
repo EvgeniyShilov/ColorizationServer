1fcf0d112d7c2d424420e30d545aea24ce26f66b
EvgeniyShilov <eugshilov@gmail.com>
2017-05-11 23:16:56 +0300

    made quick fix

--- server.py
+++ server.py
@@ -8,10 +8,10 @@ def load():
     img = request.body.read()
     id = request.get_header("id")
     token = request.get_header("token")
-    f = open("./storage/input/" + id + ".jpg", "wb")
+    f = open("./storage/input/" + id + ".png", "wb")
     f.write(img.decode('base64'))
     f.close()
-    colorize.colorize("./storage/input/" + id + ".jpg", "./storage/output/" + id + ".png", token)
+    colorize.colorize("./storage/input/" + id + ".png", "./storage/output/" + id + ".png", token)
 
 @get("/get")
 def get():
