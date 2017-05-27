e2629eeef6b05c648326164aac2e4f9c62f6b16d
EvgeniyShilov <eugshilov@gmail.com>
2017-05-27 17:31:46 +0300

    cleaned project

--- neuralnet/colorization.py
+++ neuralnet/colorization.py
@@ -20,34 +20,31 @@ def colorize(id, token):
 	
 	caffe.set_mode_cpu()
 
-	# Select desired model
 	net = caffe.Net(prototxt, caffemodel, caffe.TEST)
 
-	(H_in, W_in) = net.blobs['data_l'].data.shape[2:] # get input shape
-	(H_out, W_out) = net.blobs['class8_ab'].data.shape[2:] # get output shape
+	(H_in, W_in) = net.blobs['data_l'].data.shape[2:]
+	(H_out, W_out) = net.blobs['class8_ab'].data.shape[2:]
 
-	pts_in_hull = np.load('./neuralnet/resource.npy') # load cluster centers
-	net.params['class8_ab'][0].data[:, :, 0, 0] = pts_in_hull.transpose((1, 0)) # populate cluster centers as 1x1 convolution kernel
+	pts_in_hull = np.load('./neuralnet/resource.npy')
+	net.params['class8_ab'][0].data[:, :, 0, 0] = pts_in_hull.transpose((1, 0))
 
-	# load the original image
 	img_rgb = caffe.io.load_image(img_in)
 
-	img_lab = color.rgb2lab(img_rgb) # convert image to lab color space
-	img_l = img_lab[:, :, 0] # pull out L channel
-	(H_orig, W_orig) = img_rgb.shape[:2] # original image size
+	img_lab = color.rgb2lab(img_rgb)
+	img_l = img_lab[:, :, 0]
+	(H_orig, W_orig) = img_rgb.shape[:2]
 
-	# resize image to network input size
-	img_rs = caffe.io.resize_image(img_rgb, (H_in, W_in)) # resize image to network input size
+	img_rs = caffe.io.resize_image(img_rgb, (H_in, W_in))
 	img_lab_rs = color.rgb2lab(img_rs)
 	img_l_rs = img_lab_rs[:, :, 0]
 
-	net.blobs['data_l'].data[0, 0, :, :] = img_l_rs - 50 # subtract 50 for mean-centering
-	net.forward() # run network
+	net.blobs['data_l'].data[0, 0, :, :] = img_l_rs - 50
+	net.forward()
 
-	ab_dec = net.blobs['class8_ab'].data[0, :, :, :].transpose((1, 2, 0)) # this is our result
-	ab_dec_us = sni.zoom(ab_dec, (1.*H_orig / H_out, 1.*W_orig / W_out, 1)) # upsample to match size of original image L
-	img_lab_out = np.concatenate((img_l[:, :, np.newaxis], ab_dec_us), axis = 2) # concatenate with original image L
-	img_rgb_out = (255 * np.clip(color.lab2rgb(img_lab_out), 0, 1)).astype('uint8') # convert back to rgb
+	ab_dec = net.blobs['class8_ab'].data[0, :, :, :].transpose((1, 2, 0))
+	ab_dec_us = sni.zoom(ab_dec, (1.*H_orig / H_out, 1.*W_orig / W_out, 1))
+	img_lab_out = np.concatenate((img_l[:, :, np.newaxis], ab_dec_us), axis = 2)
+	img_rgb_out = (255 * np.clip(color.lab2rgb(img_lab_out), 0, 1)).astype('uint8')
 
 	plt.imsave(img_out, img_rgb_out)
 
--- neuralnet/fetch.sh
+++ neuralnet/fetch.sh
@@ -1,2 +1,1 @@
-
 wget http://eecs.berkeley.edu/~rich.zhang/projects/2016_colorization/files/demo_v2/colorization_release_v2.caffemodel -O ./release.caffemodel
